833ed8df67dc8c9134e73bb0e7c7c21e5ba7975c
Gabriel Botelho <botelhog275@gmail.com>
2022-05-15 10:12:19 -0300

    Mudando a velocidade e perspectiva da camera

--- src/SceneManager.js
+++ src/SceneManager.js
@@ -1,6 +1,6 @@
 import * as THREE from 'three';
 
-var speed = 0.2
+var speed = 0.08
 var vectorDirection = new THREE.Vector3(0, 0.6442, -0.7648);
 
 export function update(camera, airplane, animationOn) {
